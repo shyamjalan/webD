8ec82e66a20b2ffffd1e8c600d421c16888a400d
shyamjalan <shyamjalan.bitmesra@gmail.com>
2020-02-11 00:56:50 +0530

    Until Multiple File Server

--- nodews/basichttpserver/index.js
+++ nodews/basichttpserver/index.js
@@ -5,14 +5,25 @@ const fs = require("fs"); //Used for reading/writing files
 function requestHandler(request, response) {
   console.log(request.url);
   response.writeHead(200, { "content-type": "text/html" });
-  fs.readFile("./index.html", function(err, data) {
+  let filePath;
+
+  switch (request.url) {
+    case "/":
+      filePath = "./index.html";
+      break;
+    case "/profile":
+      filePath = "./profile.html";
+      break;
+    default:
+      filePath = "./404.html";
+  }
+  fs.readFile(filePath, function(err, data) {
     if (err) {
       console.log("error: ", err);
       return response.end("<h1>Error!</h1>");
     }
     return response.end(data);
   });
-  // response.end("<h1>Gotcha!</h1>");
 }
 
 const server = http.createServer(requestHandler);
