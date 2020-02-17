36cc3954ac44f25745e4b8b26bc80ceddecf721a
shyamjalan <shyamjalan.bitmesra@gmail.com>
2020-02-18 01:29:43 +0530

    Completed upto Styling the App

--- nodews/contact_list/index.js
+++ nodews/contact_list/index.js
@@ -6,20 +6,61 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded());
+//Accessing Static Files
+app.use(express.static('assets'))
+
+//MiddleWare 1
+// app.use(function(request, response, next) {
+//   console.log("middleware 1 called");
+//   next();
+// });
+
+//MiddleWare 2
+// app.use(function(request, response, next) {
+//   console.log("middleware 2 called");
+//   next();
+// });
+
+var contactList = [
+  {
+    name: "Shyam",
+    phone: "8953171545"
+  },
+  {
+    name: "Kishu",
+    phone: "7458990334"
+  },
+  {
+    name: "Bata",
+    phone: "7007150981"
+  }
+];
 
 app.get("/", function(request, response) {
   // console.log(__dirname);
 
   // response.send("<h1>It's running!!</h1>");
   return response.render("home", {
-    title: "Contacts List"
+    title: "Contacts List",
+    contact_list: contactList
   });
 });
 
-app.get("/practice", function(request, response) {
-  return response.render("practice", {
-    title: "Let's play with ejs"
-  });
+// app.get("/practice", function(request, response) {
+//   return response.render("practice", {
+//     title: "Let's play with ejs"
+//   });
+// });
+
+app.post("/create-contact", function(request, response) {
+  // contactList.push({
+  //   name: request.body.name,
+  //   phone: request.body.phone
+  // });
+  contactList.push(request.body);
+  // return response.redirect("/");
+  return response.redirect("back");
 });
 
 app.listen(port, function(err) {
