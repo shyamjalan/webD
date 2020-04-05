8d494f5a6eb271d3fcf2cb18e67d594bba251768
s0j01pv <Shyam.Jalan@walmartlabs.com>
2020-04-06 00:54:48 +0530

    Static Files setup for individual pages done

--- nodews/codeial/index.js
+++ nodews/codeial/index.js
@@ -7,6 +7,10 @@ app.use(express.static('./assets'))
 
 app.use(expressLayouts);
 
+//extract style and script from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 //use express router
 app.use('/', require('./routes'));
 
