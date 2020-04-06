9aef79e89e90ae91a2ca0c48905a48880f5988ba
s0j01pv <Shyam.Jalan@walmartlabs.com>
2020-04-06 22:42:25 +0530

    Create sign in and sign up forms

--- nodews/codeial/controllers/users_controller.js
+++ nodews/codeial/controllers/users_controller.js
@@ -17,3 +17,13 @@ module.exports.signIn = function(request, response) {
         title : 'Codeial | Sign In'
     })
 }
+
+//get the sign up data
+module.exports.create = function(request,response){
+    //TODO Later
+}
+
+//sign in and create a session for the user
+module.exports.createSession = function(request,response){
+    //TODO Later
+}
