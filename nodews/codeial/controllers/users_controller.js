eaebe77821ee77fd7885a14632c4e45a39a105ba
s0j01pv <Shyam.Jalan@walmartlabs.com>
2020-04-06 22:33:38 +0530

    Sign In and Sign Up Views Created

--- nodews/codeial/controllers/users_controller.js
+++ nodews/codeial/controllers/users_controller.js
@@ -3,3 +3,17 @@ module.exports.profile = function (request, response) {
         title: 'Profile'
     })
 }
+
+//Render the sign up page
+module.exports.signUp = function(request, response) {
+    return response.render('user_sign_up', {
+        title : 'Codeial | Sign Up'
+    })
+}
+
+//Render the sign in page
+module.exports.signIn = function(request, response) {
+    return response.render('user_sign_in', {
+        title : 'Codeial | Sign In'
+    })
+}
--- nodews/codeial/routes/users.js
+++ nodews/codeial/routes/users.js
@@ -6,5 +6,7 @@ const postsController = require('../controllers/posts_controller');
 
 router.get('/profile', usersController.profile);
 router.get('/posts', postsController.posts);
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
 
 module.exports = router;
