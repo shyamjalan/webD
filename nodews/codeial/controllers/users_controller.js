1f638512ce3a7349129fae546fd00f10654b88ae
s0j01pv <Shyam.Jalan@walmartlabs.com>
2020-04-07 01:07:46 +0530

    Sign Up Implemented

--- nodews/codeial/controllers/users_controller.js
+++ nodews/codeial/controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function (request, response) {
     response.render('users',{
         title: 'Profile'
@@ -20,7 +22,27 @@ module.exports.signIn = function(request, response) {
 
 //get the sign up data
 module.exports.create = function(request,response){
-    //TODO Later
+    if(request.body.password != request.body.confirm_password){
+        return response.redirect('back');
+    }
+    User.findOne({email: request.body.email}, function(err, user){
+        if(err){
+            console.log('Error in finding user in signing up');
+            return;
+        }
+        if(!user){
+            User.create(request.body, function(err,user){
+                if(err){
+                    console.log('Error in creating user while signing up');
+                    return;
+                }
+                return response.redirect('/users/sign-in');
+            })
+        }
+        else{
+            return response.redirect('back');
+        }
+    })
 }
 
 //sign in and create a session for the user
