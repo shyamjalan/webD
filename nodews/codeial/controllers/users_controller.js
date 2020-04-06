ba921c939c5d7f92598b1a065aa36e1cbdc00488
s0j01pv <Shyam.Jalan@walmartlabs.com>
2020-04-07 02:58:06 +0530

    Sign Out implemented and restrcited access to profile page

--- nodews/codeial/controllers/users_controller.js
+++ nodews/codeial/controllers/users_controller.js
@@ -1,23 +1,40 @@
 const User = require('../models/user');
 
 module.exports.profile = function (request, response) {
-    response.render('users',{
-        title: 'Profile'
-    })
+    if(request.cookies.user_id){
+        User.findById(request.cookies.user_id, function(err, user){
+            if(err){
+                console.log('Error in finding user in Profile loading ',err);
+                return;
+            }
+            if(user){
+                return response.render('users',{
+                    title: 'User Profile',
+                    user: user
+                });
+            }
+            else{
+                return response.redirect('/users/sign-in');
+            }
+        });
+    }
+    else{
+        return response.redirect('/users/sign-in');
+    }
 }
 
 //Render the sign up page
 module.exports.signUp = function(request, response) {
     return response.render('user_sign_up', {
         title : 'Codeial | Sign Up'
-    })
+    });
 }
 
 //Render the sign in page
 module.exports.signIn = function(request, response) {
     return response.render('user_sign_in', {
         title : 'Codeial | Sign In'
-    })
+    });
 }
 
 //get the sign up data
@@ -27,13 +44,13 @@ module.exports.create = function(request,response){
     }
     User.findOne({email: request.body.email}, function(err, user){
         if(err){
-            console.log('Error in finding user in signing up');
+            console.log('Error in finding user in signing up ',err);
             return;
         }
         if(!user){
             User.create(request.body, function(err,user){
                 if(err){
-                    console.log('Error in creating user while signing up');
+                    console.log('Error in creating user while signing up ',err);
                     return;
                 }
                 return response.redirect('/users/sign-in');
@@ -50,7 +67,7 @@ module.exports.createSession = function(request,response){
     //find the user
     User.findOne({email : request.body.email}, function(err, user){
         if(err){
-            console.log('Error in finding user in signing in');
+            console.log('Error in finding user in signing in ',err);
             return;
         }
         //handle user found
@@ -67,6 +84,10 @@ module.exports.createSession = function(request,response){
         //handle user not found
         return response.redirect('back');
         }
-    });
-    
+    });   
+}
+
+module.exports.deleteSession = function(request,response){
+    response.clearCookie('user_id');
+    return response.redirect('/users/sign-in');
 }
