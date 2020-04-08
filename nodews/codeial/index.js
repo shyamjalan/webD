4fb1b4c087b37945c13ae165378d54bf645ebfba
s0j01pv <Shyam.Jalan@walmartlabs.com>
2020-04-09 02:09:08 +0530

    connect-mongo setup done

--- nodews/codeial/index.js
+++ nodews/codeial/index.js
@@ -8,6 +8,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
 
 app.use(express.urlencoded());
 
@@ -25,6 +26,7 @@ app.set('layout extractScripts', true);
 app.set('view engine','ejs');
 app.set('views','./views');
 
+//mongo store is used to store the session cookie in the db
 app.use(session({
   name: 'codeial',
   //TODO change the secret before deployment
@@ -33,7 +35,16 @@ app.use(session({
   resave: false,
   cookie: {
     maxAge: (1000 * 60 * 100)
-  }
+  },
+  store: new MongoStore(
+    {
+      mongooseConnection: db,
+      autoRemove: 'disabled'
+    },
+    function(err){
+      console.log(err || "connect-mongodb setup ok");
+    }
+  )
 }));
 
 app.use(passport.initialize());
