aa07f54202f45a38208027db0383b470aa50643a
s0j01pv <Shyam.Jalan@walmartlabs.com>
2020-04-13 00:07:59 +0530

    Posts Controller Added and setup
    Now, user can add Posts from home screen

---
+++ nodews/codeial/controllers/posts_controller.js
@@ -0,0 +1,14 @@
+const Post = require('../models/post');
+
+module.exports.create = function(request,response){
+    Post.create({
+        content : request.body.content,
+        user : request.user._id
+    }, function(err, post){
+        if(err){
+            console.log('Error in creating a post');
+            return;
+        }
+        return response.redirect('back');
+    });
+}
