29a6bf5102a4e68c85c9013775fe6cc104dd3252
s0j01pv <Shyam.Jalan@walmartlabs.com>
2020-04-13 00:30:38 +0530

    Showing all the posts along with user data from db

--- nodews/codeial/controllers/home_controller.js
+++ nodews/codeial/controllers/home_controller.js
@@ -1,8 +1,31 @@
+const Post = require('../models/post');
+
 module.exports.home = function (request, response) {
     // console.log(request.cookies);
     // response.cookie('user_id', 25);
-    return response.render('home', {
-        title: "Home"
+
+    // Show all the posts in the db
+    // Post.find({}, function(err, posts){
+    //     if(err){
+    //         console.log('Error in fetching posts!');
+    //         return;
+    //     }
+    //     return response.render('home', {
+    //         title: "Codeial | Home",
+    //         posts : posts
+    //     });
+    // });
+
+    // Show all the posts along with user data from db
+    Post.find({}).populate('user').exec(function(err,posts){
+        if(err){
+            console.log('Error in fetching posts!');
+            return;
+        }
+        return response.render('home', {
+            title: "Codeial | Home",
+            posts : posts
+        });
     });
 }
 
