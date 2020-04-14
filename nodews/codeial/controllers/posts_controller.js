e9a0feeef16280f732971a2fbb1d1ea67a196099
s0j01pv <Shyam.Jalan@walmartlabs.com>
2020-04-14 23:19:52 +0530

    Added code to delete posts

--- nodews/codeial/controllers/posts_controller.js
+++ nodews/codeial/controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function(request,response){
     Post.create({
@@ -10,5 +11,24 @@ module.exports.create = function(request,response){
             return;
         }
         return response.redirect('back');
+    });
+}
+
+module.exports.destroy = function(request,response){
+    Post.findById(request.params.id, function(err, post){
+        if(err){
+            console.log('Error in finding the post');
+            return;
+        }
+        //.id means converting the object id into string
+        if(post.user == request.user.id){
+            post.remove();
+            Comment.deleteMany({post : request.params.id}, function(err){
+                return response.redirect('back');
+            });
+        }
+        else{
+            return response.redirect('back');
+        }
     });
 }
