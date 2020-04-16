cf02b5942a03b3bcde88cf90aeda2ebee9be83c2
s0j01pv <Shyam.Jalan@walmartlabs.com>
2020-04-16 23:06:36 +0530

    Update home_controller.js
    Converted home function to async await

--- nodews/codeial/controllers/home_controller.js
+++ nodews/codeial/controllers/home_controller.js
@@ -1,7 +1,7 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function (request, response) {
+module.exports.home = async function (request, response) {
     // console.log(request.cookies);
     // response.cookie('user_id', 25);
 
@@ -18,32 +18,29 @@ module.exports.home = function (request, response) {
     // });
 
     // Show all the posts along with user data from db
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err,posts){
-        if(err){
-            console.log('Error in fetching posts!');
-            return;
-        }
-        User.find({}, function (err, users) {
-            if(err){
-                console.log('Error in funding all users');
-                return;
+
+    try{
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
             }
-            return response.render('home', {
-                title: "Codeial | Home",
-                posts : posts,
-                all_users : users
-            });
-        })
-        
-    });
+        });
+
+        let users = await User.find({});
+
+        return response.render('home', {
+            title: "Codeial | Home",
+            posts : posts,
+            all_users : users
+        });
+    }catch(err){
+        console.log('Error ', err);
+        return;
+    }
+    
 }
 
 // module.exports.actionName = function(request,response){}
