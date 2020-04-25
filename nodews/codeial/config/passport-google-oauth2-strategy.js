16d14160c29e7f5a0b95c021cd58430a7e895499
s0j01pv <Shyam.Jalan@walmartlabs.com>
2020-04-25 23:46:00 +0530

    Installed google-oauth and setup done

---
+++ nodews/codeial/config/passport-google-oauth2-strategy.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const googleStartegy = require('passport-google-oauth').OAuth2Strategy;
+const crypto = require('crypto');
+const User = require('../models/user');
+
+//Tell passport to use the particular strategy for google login
+passport.use(new googleStartegy({
+        clientID: "19230590553-dr7k99mncitekf9jp7n0juticomm53a4.apps.googleusercontent.com",
+        clientSecret: "gGV3XkyM_xAtRAFSSwbJq5e8",
+        callbackURL: "http://localhost:8000/users/auth/google/callback"
+    },
+    function(accessToken, refreshToken, profileInfo, done){
+        //Find a User
+        User.findOne({email: profileInfo.emails[0].value}).exec(function(err, user){
+            if(err){
+                console.log("Error in google strategy-passport",err);
+                return;
+            }
+            console.log(profileInfo);
+            if(user){
+                // If found, set this user as request.user
+                return done(null, user);
+            }
+            else{
+                // If not found, create the user and set it as request.user
+                User.create({
+                    name: profileInfo.displayName,
+                    email: profileInfo.emails[0].value,
+                    password: crypto.randomBytes(20).toString('hex');
+                }, function(err, user){
+                    if(err){
+                        console.log("Error in google strategy-passport",err);
+                        return;
+                    }
+                    return done(null, user);
+                });
+            }
+        });
+    }
+));
+
+module.exports = passport;
