a24ba90785fdf7a844db2371a3afb5e3392d1406
s0j01pv <Shyam.Jalan@walmartlabs.com>
2020-05-10 23:22:54 +0530

    Update chat_engine.js

--- nodews/codeial/assets/js/chat_engine.js
+++ nodews/codeial/assets/js/chat_engine.js
@@ -3,7 +3,7 @@ class ChatEngine{
         this.chatBox = $(`#${chatBoxID}`);
         this.userEmail = userEmail;
 
-        this.socket = io.connect('http://localhost:5000');
+        this.socket = io.connect('http://13.235.68.200:5000');
         if(this.userEmail){
             this.connectionHandler();
         }
